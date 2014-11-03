43fddd2eee50f906af59b97a27125ab180e127b0
n38803 <shaunthompson@fullsail.edu>
2014-11-02 19:14:19 -0500

    Removed app/Added custom functions/testing listview
    Removed application with errors. Attempting to utilize listview with
    array data... failing miserably. Setup data collection via custom
    functions

--- Java1411/app/src/main/java/android/fullsail/com/java1411/MyActivity.java
+++ Java1411/app/src/main/java/android/fullsail/com/java1411/MyActivity.java
@@ -4,18 +4,74 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import view.View;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Textview;
+import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.AlertDialog;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 
 
 public class MyActivity extends Activity {
 
+    // user input variables
+    private TextView inputText;
+
+    // data collection variables
+    final LinkedHashSet<String> inputList = new LinkedHashSet<String>();
+    String complete = "";
+
+    // math equation variables
+    int listLength;
+    int charLength;
+    int average;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+
+
+        // assign input to data references for collection
+        inputText = (TextView) findViewById(R.id.userInput);
+
+
+        // assign button reference
+        Button button = (Button) findViewById(R.id.submitButton);
+
+        // event listener for button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // add user text to hash set & clear visible entry
+                inputList.add(inputText.getText().toString());
+                clearInput();
+
+                // concatenate strings within set
+                concatenateFunction();
+
+                // determine average length of characters
+                mathFunction();
+
+
+
+
+
+
+
+            }
+
+        });
     }
 
 
@@ -37,4 +93,33 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // CUSTOM FUNCTIONS ---------------------
+
+    // clear user input
+    private void clearInput() {
+        inputText.setText("");
+    }
+
+    private void concatenateFunction (){
+
+        // Concatenate all strings in the list
+        for(String string : inputList) {
+            complete += string;
+        }
+
+    }
+    private void mathFunction (){
+
+        // set length
+        listLength = inputList.size();
+
+        // character count
+        charLength = complete.length();
+
+        // average character count
+        average = charLength / listLength;
+
+    }
+
 }
