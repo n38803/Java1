f4cc52d438d9e3528d584888c1503ab6b2775d21
n38803 <shaunthompson@fullsail.edu>
2014-11-16 22:52:27 -0500

    FINALLY Complete :) So many bluescreens :(

--- Java1411-W3/app/src/main/java/android/fullsail/com/java1411_w3/MyActivity.java
+++ Java1411-W3/app/src/main/java/android/fullsail/com/java1411_w3/MyActivity.java
@@ -1,24 +1,15 @@
 package android.fullsail.com.java1411_w3;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -35,58 +26,21 @@ public class MyActivity extends Activity {
     private ListView hListView;
     public TextView hText;
     public int selected;
+    final HashMap<Integer, Handset> hMap = new HashMap<Integer, Handset>();
 
-    @Override
-    public void onConfigurationChanged(Configuration myConfig){
-        // orientation detection
-        int orientation = getWindowManager().getDefaultDisplay().getRotation();
-        Log.d("DEBUGGING: ", "Orientation Change");
-
-        // if orientation has not changed from vertical
-        if(orientation == 0 || orientation == 180)
-        {
-            setContentView(R.layout.activity_my);
-            Log.d("DEBUGGING", "Orientation: " + orientation);
-        }
-
-        // if orientation has shifted to horizontal
-        else if(orientation == 90 || orientation == 270)
-        {
-            setContentView(R.layout.landscape);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_my);
 
         // orientation detection
         int orientation = getWindowManager().getDefaultDisplay().getRotation();
-
-
-
-        // if orientation has not changed from vertical
-        if(orientation == 0 || orientation == 180)
-        {
-            setContentView(R.layout.activity_my);
-            Log.d("DEBUGGING", "Orientation: " + orientation);
-        }
-
-        // if orientation has shifted to horizontal
-        else if(orientation == 90 || orientation == 270)
-        {
-            setContentView(R.layout.landscape);
-        }
-
 
         selected = 0;
 
         handsetArrayList = new ArrayList<Handset>();
         ArrayAdapter<String> hAdapter;
-
-        final HashMap<Integer, Handset> hMap = new HashMap<Integer, Handset>();
-        hText = (TextView) findViewById(R.id.textView);
 
                 // assign object information
                 Handset noselection = new Handset();
@@ -120,81 +74,89 @@ public class MyActivity extends Activity {
                 hMap.put(4, notefour);
 
                 Handset noteedge = new Handset();
-                noteedge.setName("Snapdragon 805");
-                noteedge.setProcessor("Snapdragon");
+                noteedge.setName("Galaxy Note Edge");
+                noteedge.setProcessor("Snapdragon 805");
                 noteedge.setRelease("Nov. 2014");
                 hMap.put(5, noteedge);
 
 
-        // assign views
-        hspinner = (Spinner) findViewById(R.id.spinner);
-        hListView = (ListView) findViewById(R.id.listView);
+
+        if (orientation == 0)
+        {
+            // assign views
+            hspinner = (Spinner) findViewById(R.id.spinner);
+            hText = (TextView) findViewById(R.id.textView);
+
+            // Adapter declaration
+            ArrayList<String> hList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.handsets)));
+            hAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, hList);
+
+            // set adapter
+            hspinner.setAdapter((hAdapter));
 
 
-        // Adapter declaration
-        ArrayList<String> hList = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.handsets)));
-        hAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, hList);
+
+            // spinner event listener
+            hspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
 
+                    // grabs selected item's position
+                    selected = hspinner.getSelectedItemPosition();
+                    setText();
 
-        // set adapters
-        hspinner.setAdapter((hAdapter));
-        //hListView.setAdapter((hAdapter));
+                }
 
-        // spinner event listener
-        hspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
 
+                    hText.setText("Please make a selection.");
 
-                // grabs selected item's position
-                selected = hspinner.getSelectedItemPosition();
-                hText.setText("DEVICE NAME:\n\n" + hMap.get(selected).getName()
-                                + "\n\n\nPROCESSOR:\n\n" + hMap.get(selected).getProcessor()
-                                + "\n\n\nRELEASE DATE:\n\n" + hMap.get(selected).getRelease()
-                );
+                }
+            });
+        }
+        else if (orientation == 1)
+        {
 
-            }
+            setContentView(R.layout.landscape);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            hListView = (ListView) findViewById(R.id.landlistview);
+            hText = (TextView) findViewById(R.id.landtextView);
 
-                hText.setText("Please make a selection.");
-
-            }
-        });
-
-        /*
-        // list event listener
-        hListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            // Adapter declaration
+            ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Arrays.asList(getResources().getStringArray(R.array.handsets)));
 
 
-                // grabs selected item's position
-                selected = hListView.getSelectedItemPosition();
-                hText.setText("DEVICE NAME:\n" + handsetArrayList.get(selected).getName()
-                                + "\n\nPROCESSOR:\n" + handsetArrayList.get(selected).getProcessor()
-                                + "\n\nRELEASE DATE:\n" + handsetArrayList.get(selected).getRelease()
-                );
+            hListView.setAdapter(arrayAdapter);
 
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            hListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                 @Override
+                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                hText.setText("Please make a selection.");
+                    selected = i;
+                    setText();
 
-            }
-        });
-        */
+                 }
+             });
+
+        }
+
+
 
 
 
 
     }
 
+    public void setText(){
 
+        hText.setText("DEVICE NAME:\n\n" + hMap.get(selected).getName()
+                        + "\n\n\nPROCESSOR:\n\n" + hMap.get(selected).getProcessor()
+                        + "\n\n\nRELEASE DATE:\n\n" + hMap.get(selected).getRelease()
+        );
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
